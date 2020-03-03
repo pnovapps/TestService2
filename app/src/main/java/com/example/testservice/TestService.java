961c6ab38434c37c3e361ec8d4d8cdc852421d8a
peter.nowell <peter.nowell@vigilantapps.com>
2020-03-03 13:45:18 +0000

    Test code for accessibilityservice upgrade bug 2

--- app/src/main/java/com/example/testservice/TestService.java
+++ app/src/main/java/com/example/testservice/TestService.java
@@ -1,35 +1,35 @@
 package com.example.testservice;
 
 import android.accessibilityservice.AccessibilityService;
-import android.annotation.SuppressLint;
-import android.graphics.PixelFormat;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.os.Build;
 import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 
-import androidx.annotation.RequiresApi;
+import static android.accessibilityservice.AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES;
 
 public class TestService extends AccessibilityService {
 
     private static final String TAG = TestService.class.getSimpleName();
 
-    private static TestService thisInstance;
-
-    public static TestService getInstance() {
-        return thisInstance;
-    }
-
     public TestService() {
         super();
         Log.d(TAG, "TestService");
-        thisInstance = this;
     }
 
     @Override
     protected void onServiceConnected() {
+
         Log.d(TAG, "onServiceConnected");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            final AccessibilityServiceInfo info = getServiceInfo();
+            final int capabilities = info.getCapabilities();
+            Log.d(TAG, "flags=0x" + Integer.toHexString(capabilities));
+            Log.d(TAG, "canPerformGestures=" +
+                    ((capabilities & CAPABILITY_CAN_PERFORM_GESTURES) != 0x0));
+        }
+
     }
 
     @Override
@@ -46,50 +46,6 @@ public class TestService extends AccessibilityService {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
-    }
-
-    @SuppressLint("InflateParams")
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
-    public void showAccessibilityOverlay(View accessibilityOverlay) {
-
-        Log.d(TAG, "showAccessibilityOverlay");
-
-        WindowManager windowManager = (WindowManager) this.getSystemService(WINDOW_SERVICE);
-
-        if(windowManager == null) {
-            return;
-        }
-
-        final WindowManager.LayoutParams layoutParams
-                = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.OPAQUE);
-
-        // If the update fails to restart the accessibility service correctly an exception
-        // is thrown as below
-        //
-        // 02-26 09:55:48.146  5800  6759 W WindowManager: Attempted to add Accessibility overlay window with unknown token null.  Aborting.
-        // 02-26 09:55:48.147 29577 29577 E MainActivity: android.view.WindowManager$BadTokenException: Unable to add window -- token null is not valid; is your activity running?
-        windowManager.addView(accessibilityOverlay, layoutParams);
-
-    }
-
-    public void hideAccessibilityOverlay(View accessibilityOverlay) {
-
-        Log.d(TAG, "hideAccessibilityOverlay " + accessibilityOverlay);
-
-        WindowManager windowManager = (WindowManager) this.getSystemService(WINDOW_SERVICE);
-
-        if(windowManager == null) {
-            return;
-        }
-
-        windowManager.removeView(accessibilityOverlay);
-
     }
 
 }
